a33f0aeb5a17d779e130545ce7b2d54a139f0d4b
OceansBlue <OceansBlueCode@gmail.com>
2018-12-16 22:42:24 -0600

    more new lines

--- Tic_Tac_Toe/Tic_Tac_Toe.py
+++ Tic_Tac_Toe/Tic_Tac_Toe.py
@@ -1,6 +1,38 @@
 # Tic Tac Toe game in Python
 
-board = [' ' for x in range(10) ]
+# This line is to create the board to store the X & O for the game.
+# start with ' ' to give a leading space. 
+board = [' ' for x in range(10) ] # using Ten, to get the user to enter numbers from 1 to 9
 
-def inserLetter(letter, pos):
-    board[pos] == ' '
+def inserLetter(letter, pos):  # this is to insert the letter into the board.
+    board[pos] == letter
+
+def spaceIsFree(pos):   # to check if the space is free or not before inserting the letter.
+    return board[pos] == ' '   # == will give us if it is true or false value.
+
+# to print the board to be used for the X, O game
+def printBaord (board):
+    print('  |  |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board [3])
+    print('  |  |')
+    print('----------')
+    print('  |  |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board [6])
+    print('  |  |')
+    print('----------')
+    print('  |  |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board [9])
+    print('  |  |')
+    print('----------')
+    print('  |  |')
+    
+def isWinner (bo, le):  # bo stands for Board
+    return (bo [7] == le and bo[8] == le and bo[9] == le) or
+           (bo [4] == le and bo[5] == le and bo[6] == le) or
+           (bo [1] == le and bo[2] == le and bo[3] == le) or
+           (bo [1] == le and bo[4] == le and bo[7] == le) or
+           (bo [2] == le and bo[5] == le and bo[8] == le) or
+           (bo [3] == le and bo[6] == le and bo[9] == le) or
+           (bo [1] == le and bo[5] == le and bo[9] == le) or
+           (bo [3] == le and bo[5] == le and bo[7] == le) 
+
